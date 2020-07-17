d72998ee0f881292a921a16eac233608841d5a46
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-17 02:08:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,14 @@
 require 'pry'
 
 def key_for_min_value(name_hash)
-  key=500
-  name_hash.each 
+  lowest_key = nil
+  lowest_value = nil
+  hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
+  end
+  lowest_key
+ 
 end
