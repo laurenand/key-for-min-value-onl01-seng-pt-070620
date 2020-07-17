4a04a1b04940f6c7e976a9df9604ec40671740bf
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-17 01:25:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-#hash = {:blake => 500, :ashley => 2, :adam => 1}
+
 require 'pry'
+
 def key_for_min_value(name_hash)
   
 end
